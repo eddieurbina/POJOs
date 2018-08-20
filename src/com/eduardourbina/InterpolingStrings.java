4211e62a56036ff2cf934b97a74032975f0ede44
eduardourbinaneoris <39962286+eduardourbinaneoris@users.noreply.github.com>
2018-08-20 11:56:39 -0500

    Starting a series of Java Exercises

---
+++ src/com/eduardourbina/InterpolingStrings.java
@@ -0,0 +1,12 @@
+package com.eduardourbina;
+
+public class InterpolingStrings {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "Goodbye";
+        int aLength = a.length();
+        int bLength = b.length();
+        String formatter = "%1$-";
+
+    }
+}
---
+++ src/com/eduardourbina/Main.java
@@ -0,0 +1,8 @@
+package com.eduardourbina;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
